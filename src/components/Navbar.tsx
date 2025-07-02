a0ce335532f49d0ea3a7362b4cc021662ab17010
Ali Al Rida Al Najjar <alialridaalnajjar@gmail.com>
2025-07-02 17:14:12 +0300

    1.1V Add music toggle functionality to Navbar component

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,28 @@
 import { IoVolumeMediumSharp } from "react-icons/io5";
 import { Menu } from "lucide-react";
-
+import React from "react";
 export default function Navbar() {
+  const [musicPlaying, setMusicPlaying] = React.useState(true);
+
+  const handleMusicToggle = () => {
+    setMusicPlaying(!musicPlaying);
+    const audio = new Audio("menuMusic.wav");
+
+    if (musicPlaying) {
+      audio.loop = true;
+      audio
+        .play()
+        .then(() => {
+          console.log("Music is playing");
+        })
+        .catch((error) => {
+          console.error("Error playing music:", error);
+        });
+    } else {
+      audio.pause();
+    }
+  };
+
   return (
     <div className=" bg-transparent fixed top-0 left-0 w-full z-50 flex items-center justify-between p-4 backdrop-blur-md">
       <div>Ali Najjar</div>
@@ -10,7 +31,7 @@ export default function Navbar() {
           <IoVolumeMediumSharp size={25} />
         </div>
         <div>
-          <Menu  size={25}/>
+          <Menu size={25} onClick={handleMusicToggle} />
         </div>
       </div>
     </div>
