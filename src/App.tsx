53098963b31058366bf1535260076c288668d408
Ali Al Rida Al Najjar <alialridaalnajjar@gmail.com>
2025-07-03 22:11:59 +0300

    QF

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,13 @@
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import "./App.css";
 import ContentPage from "./pages/ContentPage";
-import LoadingPage from "./pages/LoadingPage";
 import MainPage from "./pages/MainPage";
 
 function App() {
   return (
     <Router >
       <Routes>
-        <Route element={<MainPage/> } path="/MainPage" />
-        <Route element={<LoadingPage/> } path="/" />
+        <Route element={<MainPage/> } path="/" />
         <Route element={<ContentPage/>} path="/ContentPage"/>
       </Routes>
     </Router>
