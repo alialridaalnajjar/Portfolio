8a7f755aca8b116045d2d773a99efdb9be37ca63
Ali Al Rida Al Najjar <alialridaalnajjar@gmail.com>
2025-07-10 21:30:56 +0300

    1.7V Add ProjectCard component for displaying project details
    - Created a new ProjectCard component that accepts props for project name, URL, repository link, image, and description.
    - Implemented a responsive layout with Tailwind CSS for styling.
    - Included links to live projects and GitHub repositories with visual indicators.

---
+++ src/components/ProjectCard.tsx
@@ -0,0 +1,39 @@
+export default function ProjectCard({
+  name,
+  url,
+  repo,
+  img,
+  desc,
+}: {
+  name: string;
+  url: string;
+  repo: string;
+  img: string;
+}) {
+  return (
+    <div className="w-full relative pt-20">
+      <div className="bg-indigo-800 flex flex-row items-center justify-between max-w-5/9 h-50">
+        <a href={url}>
+          <img src={img} alt="Project Image" className="h-35 w-55" />
+        </a>
+      </div>
+      {/*absolute overlay */}
+      <div className="absolute bottom-0.5 right-13.25 text-white caret-transparent bg-black/30 gap-2 flex flex-col items-start justify-around p-1 pl-1.5 max-h-45 h-full">
+        <h1 className="text-xl font-semibold">{name}</h1>
+        <p className="max-w-45 text-xs font-semibold ">{desc}</p>
+        <div className="underline-offset-2  text-xs uppercase min-w-24 font-light ">
+          <h1 className="pl-2 pb-0.5">Live Now </h1>
+          <div className="bg-lime-300 w-8/10 h-0.5"></div>
+        </div>
+        <div className="underline-offset-2  text-xs uppercase min-w-24 font-light ">
+          <h1 className="pl-2 pb-0.5">Github Repo </h1>
+          <div className="bg-lime-300 w-9/10 h-0.5"></div>
+        </div>
+      </div>
+      <div hidden>
+        {repo}
+        {desc}
+      </div>
+    </div>
+  );
+}
--- src/components/ProjectSection.tsx
+++ src/components/ProjectSection.tsx
@@ -1,22 +1,56 @@
-
+import ProjectCard from "./ProjectCard";
+import Imgone from "../assets/images/ArWarehouse.png";
+import ImgTwo from"../assets/images/BakaRate.png";
 export default function ProjectSection() {
+  const projects: {
+    name: string;
+    url: string;
+    repo: string;
+    img: string ;
+    desc: string;
+    inverse?:boolean;
+  }[] = [
+    {
+      name: "Ar Warehouse",
+      url: "https://ar-warehouse.vercel.app/",
+      repo: "",
+      img: Imgone,
+      desc: "A web application for managing augmented reality assets.",
+      inverse: false,
+    }, {
+      name: "Baka Rate",
+      url: "https://ar-warehouse.vercel.app/",
+      repo: "",
+      img: ImgTwo,
+      desc: "A web application for managing augmented reality assets.",
+      inverse: false,
+    },
+  ];
+
   return (
-    <div className="bg-black h-auto min-h-screen relative caret-transparent">
- {/* About section */}
+    <div className="bg-black h-auto min-h-screen relative caret-transparent pt-12 lg:pt-20 ">
+      {/* About section */}
       <div className="flex flex-row items-center justify-center pt-10.75 lg:pt-30 lg:gap-4">
         <div className="relative">
-          <h1 className="text-4xl font-bold text-white relative z-10">Projects</h1>
+          <h1 className="text-4xl font-bold text-white relative z-10">
+            Projects
+          </h1>
           <div className="absolute top-7 left-5 w-28 h-4.5 bg-blue-700 z-0"></div>
         </div>
       </div>
 
-
-        
-
-
-
-
-
-    </div>
-  )
+    <div className="mt-10">
+      {projects.map((p, index) => (
+        <ProjectCard
+          key={index}
+          name={p.name}
+          url={p.url}
+          repo={p.repo}
+          img={p.img}
+          desc={p.desc}
+          inverse={p.inverse}
+        />
+      ))}
+    </div></div>
+  );
 }
