81b9ff0bb1f16b1aa0e99215e78ce1074c9c6a89
Ali Al Rida Al Najjar <alialridaalnajjar@gmail.com>
2025-07-11 06:11:27 +0300

    1.8V Enhance ProjectCard and ProjectSection components with improved layout and functionality

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -4,35 +4,63 @@ export default function ProjectCard({
   repo,
   img,
   desc,
+  inverse,
 }: {
   name: string;
   url: string;
   repo: string;
   img: string;
-  desc:string
+  desc: string;
+  inverse: boolean;
 }) {
   return (
-    <div className="w-full relative pt-20">
-      <div className="bg-indigo-800 flex flex-row items-center justify-between max-w-5/9 h-50">
+    <div className="w-full relative pt-20 lg:px-50">
+      <div
+        className={`bg-indigo-800 flex flex-row items-center justify-between max-w-5/9 h-50 lg:h-145 ${
+          inverse ? "ml-auto lg:ml-auto" : ""
+        }`}
+      >
         <a href={url}>
-          <img src={img} alt="Project Image" className="h-35 w-55" />
+          <img
+            src={img}
+            alt="Project Image"
+            className="h-35 w-55 lg:h-120 lg:w-200"
+          />
         </a>
       </div>
-      {/*absolute overlay */}
-      <div className="absolute bottom-0.5 right-13.25 text-white caret-transparent bg-black/30 gap-2 flex flex-col items-start justify-around p-1 pl-1.5 max-h-45 h-full">
-        <h1 className="text-xl font-semibold">{name}</h1>
-        <p className="max-w-45 text-xs font-semibold ">{desc}</p>
-        <div className="underline-offset-2  text-xs uppercase min-w-24 font-light ">
-          <h1 className="pl-2 pb-0.5">Live Now </h1>
-          <div className="bg-lime-300 w-8/10 h-0.5"></div>
-        </div>
-        <div className="underline-offset-2  text-xs uppercase min-w-24 font-light ">
-          <h1 className="pl-2 pb-0.5">Github Repo </h1>
-          <div className="bg-lime-300 w-9/10 h-0.5"></div>
-        </div>
+
+      {/* Overlays here */}
+      <div
+        className={`
+        lg:max-h-100 lg:justify-around lg:bg-transparent 
+        absolute bottom-0.5 text-white caret-transparent bg-black/30 
+        gap-2 flex flex-col items-start justify-around p-1 pl-1.5 max-h-45 h-full
+        ${
+          inverse
+            ? "left-13.25 lg:left-2/10 lg:right-auto lg:top-35"
+            : "right-13.25 lg:left-auto lg:right-2/10 lg:top-35"
+        }
+      `}
+      >
+        <h1 className="text-xl font-semibold lg:text-4xl">{name}</h1>
+        <p className="max-w-45 text-xs font-semibold lg:text-xl lg:max-w-155">
+          {desc}
+        </p>
+
+        <a href={url}>
+          <div className="underline-offset-2 text-xs uppercase min-w-24 font-light">
+            <h1 className="pl-2 pb-0.5 lg:text-2xl">Live Now</h1>
+            <div className="bg-lime-300 w-8/10 h-0.5 lg:w-11/10"></div>
+          </div>
+        </a>
+
+        <a href={repo}>
+          <div className="underline-offset-2 text-xs uppercase min-w-24 font-light">
+            <h1 className="pl-2 pb-0.5 lg:text-2xl">Github Repo</h1>
+            <div className="bg-lime-300 w-9/10 h-0.5 lg:w-11/10"></div>
+          </div>
+        </a>
       </div>
-      <div hidden>
-      {repo}{desc}</div>
     </div>
   );
 }
--- src/components/ProjectSection.tsx
+++ src/components/ProjectSection.tsx
@@ -1,34 +1,36 @@
 import ProjectCard from "./ProjectCard";
 import Imgone from "../assets/images/ArWarehouse.png";
-import ImgTwo from"../assets/images/BakaRate.png";
+import ImgTwo from "../assets/images/BakaRate.png";
 export default function ProjectSection() {
   const projects: {
     name: string;
     url: string;
     repo: string;
-    img: string ;
+    img: string;
     desc: string;
-    inverse?:boolean;
+    inverse: boolean;
   }[] = [
     {
       name: "Ar Warehouse",
       url: "https://ar-warehouse.vercel.app/",
-      repo: "",
+      repo: "https://github.com/alialridaalnajjar/AR_Warehouse",
       img: Imgone,
-      desc: "A web application for managing augmented reality assets.",
+      desc: "A modern,responsive e-commerce web app for esports gadgets, built with React, TypeScript, and Tailwind CSS.",
       inverse: false,
-    }, {
+    },
+    {
       name: "Baka Rate",
-      url: "https://ar-warehouse.vercel.app/",
-      repo: "",
+      url: "https://alialridaalnajjar.github.io/BakaRate/",
+      repo: "https://github.com/alialridaalnajjar/BakaRate",
       img: ImgTwo,
-      desc: "A web application for managing augmented reality assets.",
-      inverse: false,
+      desc: " BAKARATE is a modern, interactive anime listing and rating web app built with React, TypeScript, and Tailwind CSS.",
+      inverse: true,
     },
   ];
 
   return (
-    <div className="bg-black h-auto min-h-screen relative caret-transparent pt-12 lg:pt-20 ">
+    
+    <div className="bg-black h-auto min-h-screen relative caret-transparent  ">
       {/* About section */}
       <div className="flex flex-row items-center justify-center pt-10.75 lg:pt-30 lg:gap-4">
         <div className="relative">
@@ -39,18 +41,19 @@ export default function ProjectSection() {
         </div>
       </div>
 
-    <div className="mt-10">
-      {projects.map((p, index) => (
-        <ProjectCard
-          key={index}
-          name={p.name}
-          url={p.url}
-          repo={p.repo}
-          img={p.img}
-          desc={p.desc}
-          
-        />
-      ))}
-    </div></div>
+      <div className="mt-10">
+        {projects.map((p, index) => (
+          <ProjectCard
+            key={index}
+            name={p.name}
+            url={p.url}
+            repo={p.repo}
+            img={p.img}
+            desc={p.desc}
+            inverse={p.inverse}
+          />
+        ))}
+      </div>
+    </div>
   );
 }
